d0e34616bf0f2295a2390f71b157040d49490607
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-08-24 21:35:15 -0300

    Exercicio 2

---
+++ src/br/com/zup/Exercicio2.java
@@ -0,0 +1,18 @@
+package br.com.zup;
+
+public class Exercicio2 {
+    public static void main(String[] args){
+
+        float Nota1 = 9.8F;
+        float Nota2 = 8.7F;
+        float Nota3 = 9.5F;
+        float Nota4 = 7.6F;
+
+        float MediaAritmetica = (Nota1 + Nota2+ Nota3 + Nota4) / 4;
+
+        System.out.println(" A média aritimética das suas notas é:" + MediaAritmetica);
+
+
+    }
+
+}
