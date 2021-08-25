cd1823e93db139ab527cf9dd7dc6fc9a0211ea15
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-08-24 21:40:59 -0300

    Exercicio 3

---
+++ src/br/com/zup/Exercicio3.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        float Metro = 5.5F;
+        float Centimetros = Metro * 100;
+
+        System.out.println(" O valor de " + Metro + " metros," + "é a mesma coisa de " + Centimetros + " centímetros");
+    }
+}
