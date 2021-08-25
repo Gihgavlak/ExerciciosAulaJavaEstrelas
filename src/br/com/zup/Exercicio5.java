edab9fb9e4ee74f6aa34ee03d9d7e0959a7846b7
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-08-24 21:47:43 -0300

    Exercicio 5

---
+++ src/br/com/zup/Exercicio5.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class exercicio5 {
+    public static void main(String[] args){
+        int grauFahrenheit = 86;
+        float grauCelsius = (grauFahrenheit - 32) * 5/9;
+        System.out.println("O grau "+ grauFahrenheit + "ºF é o mesmo que " + grauCelsius + "ºC");
+    }
+}
